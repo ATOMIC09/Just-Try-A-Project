8f98e0aed00bc82f97d3816f1dd4b9cfe0506068
ATOMIC <66838025+ATOMIC09@users.noreply.github.com>
2021-12-04 21:25:15 +0700

    Add: Enter Language

--- voice_recognition.py
+++ voice_recognition.py
@@ -1,6 +1,8 @@
 import pyaudio
 import speech_recognition as sr
 
+language = str(input("Enter Language : "))
+print("Start!\n")
 # Select Mic
 mic_list = sr.Microphone.list_microphone_names()
 mic = sr.Microphone(1)
@@ -13,6 +15,6 @@ with mic as source:
     while True:
         audio = recog.listen(source)
         try:
-            print(recog.recognize_google(audio,language='th'))
+            print(recog.recognize_google(audio,language=language))
         except:
             continue
